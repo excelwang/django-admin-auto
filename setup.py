3f209a28a3f7f9a3d92de50a3db2f96c71484565
excelwang <wanghuajin999@163.com>
2019-04-26 17:49:40 +0800

    bump pip package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-admin-auto",
-    version = "0.1.0",
+    version = "0.1.1",
     keywords = ("pip", "django", "admin"),
     description = "To automate the Django Admin displaying.",
     long_description = open('README.rst').read(),
